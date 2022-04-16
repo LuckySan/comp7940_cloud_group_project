e17be7abe902bcf7195a25ed413465b4e39f45d4
luoxinpeng <21444455@life.hkbu.edu.hk>
2022-04-16 19:27:54 +0800

    finish search by id and search by title

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import logging
 import os
@@ -44,16 +44,47 @@ def main():
         updater.start_polling()
         updater.idle()
 
+def getData(sql):
+    with SQLConnection() as conn:
+        data = pd.read_sql(sql, conn.connector)
+        return data
 
 def echo(update, context):
     msg = update.message.text
-    #if the msg is a number, we know our user want to search dish by id
-    # if(msg.isdigit()):
-    #     with SQLConnection() as conn:
-    #         sql = f"select * from "
+    # if the msg is a number, we know our user want to search dish by id
+    if(msg.isdigit()):
+        sql = f"select * from Dishes where id = {msg}"
+        data = getData(sql)
+        title = data["title"].values[0]
+        ingredients = data["ingredients"].values[0]
+        directions = data["directions"].values[0]
+        link = data["link"].values[0]
+        ret = getStringDishByVar(title, ingredients, directions, link)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=ret, parse_mode=ParseMode.HTML)
+    # if the msg is a string, we know our user want to search dish by title
+    else:
+        sql = f"select id,title from Dishes where title like '%{msg}%' limit 0, 50"
+        data = getData(sql)
+        res = getStringDishByIdAndTitle(data)
+        res = "Do you mean these Dishes? \n" + res
+        context.bot.send_message(chat_id=update.effective_chat.id, text = res)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text= "You can input /help to see our functions")
 
+# Get All variable of dish
+def getStringDishByVar(title,ingredients,directions,link):
+    ret = f"""
+<b>Dish Title: {title}</b>\n<b>Ingredients:</b>{ingredients}\n<b>Directions:</b>{directions}\n<a href="{link}">{link}</a>
+"""
+    return ret
+
+# Get id, title of dish
+def getStringDishByIdAndTitle(data):
+    retTitle = data["title"].values
+    retId = data["id"].values
+    res = ""
+    for (i, j) in zip(retId, retTitle):
+        res += (str(i) + ". " + j + "\n")
+    return res
 
 # This method is used to retrive the chat id 
 def get_chat_id(update, context):
@@ -77,19 +108,10 @@ def get_chat_id(update, context):
 def get_dishes_of_keyWord(update: Update, context: CallbackContext)->None:
     keyword = context.args[0]
     with SQLConnection() as conn:
-        sql = f"select title from KeyWordDish,Dishes where keyword='{keyword}' and Dishes.id = KeyWordDish.dishId limit 0, 50"
+        sql = f"select Dishes.id,title from KeyWordDish,Dishes where keyword='{keyword}' and Dishes.id = KeyWordDish.dishId limit 0, 50"
         data = pd.read_sql(sql,conn.connector)
-        ret = data["title"].values
-        res = ""
-        index = 1
-        for item in ret:
-            res += (str(index) +". " + item + "\n")
-            res += ('/searchKeyWord' + item + "\n")
-            index += 1
-        a = update.message.reply_chat_action("upload_photo")
-        print(a)
-        #update.message.reply_text(res)
-
+        res = getStringDishByIdAndTitle(data)
+        update.message.reply_text(res)
 
 
 def get_amount_of_recipes(update: Update, context: CallbackContext)->None: 
